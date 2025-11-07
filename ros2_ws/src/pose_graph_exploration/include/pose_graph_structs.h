31bcdbfbd44732b3fbe9c341a32ed5ffaeda9067
mo <mampane@olin.edu>
2025-11-07 16:20:09 -0500

    Make header file for SavedLaserScan and SavedOdom structs

---
+++ ros2_ws/src/pose_graph_exploration/include/pose_graph_structs.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <cstdint>
+
+struct SavedLaserScan 
+{
+    std::vector<float> ranges;  // ranges
+    int32_t sec;   // header.stamp.sec
+    uint32_t nanosec;   // header.stamp.nanosec
+};
+
+struct SavedOdom
+{
+    int32_t sec;   // header.stamp.sec
+    uint32_t nanosec;   // header.stamp.nanosec
+
+    double position_x;  // pose.pose.position.x
+    double position_y;  // pose.pose.position.y
+    double position_z;  // pose.pose.position.z
+
+    double orientation_x; // pose.pose.orientation.x
+    double orientation_y; // pose.pose.orientation.y
+    double orientation_z; // pose.pose.orientation.z
+    double orientation_w; // pose.pose.orientation.w
+
+    double linear_x;  // twist.twist.linear.x
+    double linear_y;  // twist.twist.linear.y
+    double linear_z;  // twist.twist.linear.z
+
+    double angular_x;  // twist.twist.angular.x
+    double angular_y;  // twist.twist.angular.y
+    double angular_z;  // twist.twist.angular.z
+};
--- ros2_ws/src/pose_graph_exploration/src/pose_graph.cpp
+++ ros2_ws/src/pose_graph_exploration/src/pose_graph.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "pose_graph_structs.h"
 
 int main() {
-    std::cerr << "This works";
+    SavedLaserScan temp = {{0.1, 0.2, 0.3}, 2, 30000};
+    std::cout << temp.ranges[0] << "\n";
 }
