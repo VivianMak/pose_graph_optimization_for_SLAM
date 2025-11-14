48ccaabeb30424c67f8180308b3d38ad64f57972
mo <mampane@olin.edu>
2025-11-14 14:50:06 -0500

    Add Node class

--- ros2_ws/src/pose_graph_exploration/include/pose_graph_header.h
+++ ros2_ws/src/pose_graph_exploration/include/pose_graph_header.h
@@ -36,6 +36,30 @@ struct SavedOdom
     double angular_z;  // twist.twist.angular.z
 };
 
+class Node {
+public:
+    // Pose data
+    float x;
+    float y;
+    float theta;
+
+    // List of laser scan points (Cartesian coordinates)
+    std::vector<double> distances;
+
+    // Transformation matrix (3x3)
+    Eigen::Matrix4d transform_mat;
+
+    // Parent node (previous in sequence)
+    Node* parent;
+
+    // Begin constructor
+    Node(float x_, float y_, float theta_, Node* parent_ = nullptr)
+    : x(x_), y(y_), theta(theta_), parent(parent_) {
+    // Initialize transform as identity matrix
+    // transform_mat = {{{1.0, 0.0, 0.0},{0.0, 1.0, 0.0},{0.0, 0.0, 1.0}}};
+    }
+};
+
 void read_data(std::string filename, std::shared_ptr<std::vector<SavedLaserScan>> lidar_vector, std::shared_ptr<std::vector<SavedOdom>> odom_vector);
 std::vector<double> quaternion2euler(double x, double y, double z, double w);
 Eigen::Matrix4d pose2homogeneous(double x, double y, double z);
