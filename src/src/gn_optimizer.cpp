6883ab7af6c2dcea3a10db6b384b569a328ec441
VivianMak <vivianmak.30@gmail.com>
2025-11-29 14:30:34 -0500

    fix transform logic error

--- src/src/gn_optimizer.cpp
+++ src/src/gn_optimizer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <Eigen/Dense>
 
 #include "gn_optimizer.hpp"
@@ -9,40 +10,81 @@ namespace GN
 {
     GN::eJ computeErrorAndJacobian(const GN::Vec3 &xi, 
                                     const GN::Vec3 &xj, 
-                                    const GN::Mat33 &z_ij)
+                                    const GN::Mat33 &z_ji)
     {
         /*
         * Compute the error and Jacobian for adjacent nodes i,j
         *
-        * @param xi - ith pose
-        * @param xj - jth pose
-        * @param zij - icp transform btwn ij
+        * @param xi - ith pose (previous)
+        * @param xj - jth pose (current)
+        * @param zji - icp transform btwn ij (3x3)
         * 
         * @return eJ - error vec and Jacobian mat
         */
 
-        // initalize error and jacobian for current adjancent nodes
+        // initalize error and jacobian return struct for current adjancent nodes
         GN::eJ currenteJ;
 
         // compute error
-        currenteJ.e = gn_helper::transforms_to_dvector(xi, xj. zij);
+
+        // Invert ICP j -> i transform
+        GN::Mat33 Z_ij = z_ji.inverse();
+
+        // Turn pose i to a matrix
+        GN::Mat33 T_i = gn_helper::poseToMat(xi); 
+
+        // Compute predicted j
+        GN::Mat33 T_j_pred = T_i * Z_ij;
+
+        // Turn matrix j into a pose
+        GN::Vec3 xj_pred = gn_helper::matToPose(T_j_pred);
+
+        // Build the vector error
+        currenteJ.e(0) = xj_pred(0) - xj(0);
+        currenteJ.e(1) = xj_pred(1) - xj(1);
+        currenteJ.e(2) = utils::wrap_rad(xj_pred(0) - xj(0));
+        
+
+        // this is a transform!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+        double zji_x = zji(0), zji_y = zji(1), zji_th = zji(2);
+
+        // Rotation of node i
+        GN::Vec2 Ri;
+        Ri << cos(xi_th),  sin(xi_th),
+            -sin(xi_th),  cos(xi_th);
+
+        // building of the vectors
+        GN::Vec2 dij;
+        dij << (xj_x - xi_x),
+            (xj_y - xi_y);
+
+        GN::Vec2 zji_t;
+        zji_t << zji_x,
+                zji_y;
+
+        // error translation compoennt
+        GN::Vec2 etrans = Ri * (dij - zji_t);
+
+        // error rotation component
+        double eth = (xj_th - xi_th) - zji_th;
+
+        // error vector
+        currenteJ.e << etrans(0), etrans(1), eth;
         
         // compute jacobian
         GN::Mat36 J;
         J.setZero();
 
-        // decompose poses, use Pose struct?
+        // decpmse poses
         double xi_x = xi(0), xi_y = xi(1), xi_th = xi(2);
         double xj_x = xj(0), xj_y = xj(1), xj_th = xj(2);
 
-        // Rotation of node i
-        Eigen::Matrix2d Ri;
-        Ri << cos(xi_th),  sin(xi_th),
-            -sin(xi_th),  cos(xi_th);
 
-        // BRUH WTF
-        // d e_trans / d xi (2x2 block)
+        // d e_trans / d xi 
         J.block<2,2>(0,0) = -Ri;
+
+        // d e_trans / d xj
+        J.block<2,2>(0,3) = Ri;
 
         // d e_trans / d theta_i
         Eigen::Vector2d temp(-(xj_x - xi_x) * sin(xi_th) - (xj_y - xi_y) * cos(xi_th),
@@ -55,9 +97,6 @@ namespace GN
         // d e_theta / d xi
         J(2,2) = -1;
 
-        // d e_trans / d xj
-        J.block<2,2>(0,3) = Ri;
-
         // d e_theta / d xj
         J(2,5) = 1;
 
@@ -66,14 +105,9 @@ namespace GN
         return GN::eJ currenteJ;
     }
 
-    // void linearize()
-    // {
-
-    // }
-
     void buildLinearHb(const size_t n,
                         const std::vector<GN::Vec3> &X,  
-                        const std::vector<GN::Mat33> &Z,
+                        // const std::vector<GN::Mat33> &Z,
                         GN::dMat &H,
                         GN::dVec &b)
     {
@@ -87,15 +121,28 @@ namespace GN
         * @param b - empty size-defined vector to build
         */
 
-        for (size_t k = 0; k < n; k++){
-            int i = k;
-            int j = k+1;
+        for (size_t k = 0; k < n; k++)
+        {
+            int i = k+1;
+            int j = k;
+
+            (edges.end()-1).transform
             
             // Get the error vec and Jacobian mat
-            GN::eJ eJ = computeErrorAndJacobian(X[i], X[j], Z[k]);
+            // X[i].(edges.end()-1).transform
+            GN::eJ eJ = computeErrorAndJacobian(X[i].pose, 
+                                                X[j].pose, 
+                                                Z_[k]);
 
             GN::Vec3 e = eJ.e;
             GN::Mat36 J = eJ.J;
+
+            // Set a threshold for error
+            if (e < config_.threshold)
+            {
+                std::cout << "OPTIMIZATION MET THRESHOLD" << std::endl;
+                return true;
+            }
 
             Eigen::Matrix<double,6,6> H_ij = J.transpose() * J;
             Eigen::Matrix<double,6,1> b_ij = -J.transpose() * e;
@@ -109,20 +156,18 @@ namespace GN
             b.segment<3>(3*i) += b_ij.segment<3>(0);
             b.segment<3>(3*j) += b_ij.segment<3>(3);
             }
-        
 
+            return false;
     }
 
-    bool gnOptimizer(const std::vector Z, 
-                    cosnt std::vector<utils::Node> N, 
-                    GN_Config config)
+    bool gnOptimizer()
     {
         /*
-        * [main] Run the gauss newton global optimizer for n iterations
+        * Run the gauss newton global optimizer.
         *
         * @param Z - vector of 3x3 homogeneous matrix from ICP
         * @param N - vector of nodes, will extract 3x3 homogeneous matrix from eddge odom
-        * @param GN_Config
+        * @param config
         * 
         * @return bool - true/false for if optimizer has finished (threshold?)
         */
@@ -131,46 +176,40 @@ namespace GN
         std::clock_t start;
         start = std::clock();
 
-        std::cout << "Running Gauss-Newton Optimization..."
+        std::cout << "Running Gauss-Newton Optimization..." << std::endl;
 
-        size_t n = Z.size();
+        size_t n = Z_.size();
 
-        // intialize variables and size
-        H = GN::dMat::Zero(3*N, 3*N);
-        b = GN::dVec::Zero(3*N);
+        // Allocate space to copy nodes in reverse order
+        // We don't want to edit the nodes diretly
+        std::vector<GN::Vec3> X(N_.size());
+        std::X(N_.begin(), N_.end(), X.begin());
 
-
-        // we dont want to edit the nodes directly
-        // make a copy to perform the optimization
-        // visualize: lay X over N to show changes.
-        
-        /*
-        * TODO:
-        * - smart pointers
-        * - invert X for it to go backwards
-        * - make it a vector of poses using <utils::Poses>
-        */
-        std::vector<GN::Vec3> X = N.poses;
-
-        // GN::dMat Jacobian;
-        
-        // get the H and b coefficient matricies
-        buildLinearHb(n, X, Z, H, b);
-
-        // solving for delta x (x,y,theta)
-        GN::Vec3 dX = H.ldlt().solve(b);
-        
-
-        // SET THRESHOLD OR ITERATIONS
-
-        // update state vectors
-        for (size_t i = 0; i < X.size(); i++)
+        for (size_t j = 0; j < config_.max_iters; j++)
         {
-            X[i] += dx.segment<3>(3*i);
+            // Intialize empty matrix and vector
+            H = GN::dMat::Zero(3*N_, 3*N_);
+            b = GN::dVec::Zero(3*N_);
+            
+            // Extract the H adjancency matrix and b coefficient vector
+            bool met_threshold = buildLinearHb(n, X, H, b);
+
+            // Checking for convergence
+            if (!met_threshold)
+            {
+                // Solving for state delta X (x,y,theta)
+                GN::Vec3 dX = H.ldlt().solve(b);
+            
+                // Update state vectors
+                for (size_t i = 0; i < X.size(); i++)
+                {
+                    X[i] += dx.segment<3>(3*i);
+                }
+            } else {
+                break;
+            }
         }
-
         
-
         // Finished
         double duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
         std::cout << "Gauss-Newton Optimization Complete!" << std::endl;
